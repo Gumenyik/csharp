1a6485ef97de48f1eec3a1ca683df39b52c00a3c
Taras <taras181716@gmail.com>
2022-07-17 13:18:19 +0300

    Body program complete

--- class_animal.cs
+++ class_animal.cs
@@ -1,4 +1,13 @@
+Animal animal = new Animal{name = "Dick", kind = "Dog",age = 12, owner_name = "Taras"};
+animal.print();
 class Animal
 {
+    public string name, kind, owner_name;
+    public int age;
 
+    public void print()
+    {
+        Console.writeLine($"Information about animal.\n Owner name:{owner_name}\n Name:{name}\n Animal kind:{kind}\n Animal age:{age}\n");
+    }
 }
+
